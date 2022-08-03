780d0f11f2691662ab059b771ce7731fae20ec4b
ElivanLimaJunior <85586631+ElivanLimaJunior@users.noreply.github.com>
2022-08-03 19:31:24 -0300

    Fim da aula 4
    Neste capítulo implementamos a criação de usuários a partir de um formulário, e com isso aprendemos:
    
    A diferença entre as funções nomeadas e as funções anônimas
    A escutar eventos do browser com a função addEventListener()
    Que a função criadora de elementos é .createElement()
    A pegar o valor de um input por meio da propriedade .value
    A acessar os input de um form por meio da propriedade .name
    A adicionar elementos na página e dentro de outros elementos com a função appendChild()

--- script/script.js
+++ script/script.js
@@ -42,5 +42,41 @@ for(var i = 0; i < pacientes.length; i++) {
 const botaoAdicionar = document.querySelector("#adicionar-paciente")
 botaoAdicionar.addEventListener("click", function(event){
     event.preventDefault() //Previnindo comportamento de enviar formulário para vermos o evento ed clicar.
-    console.log("click")
+
+    // Pegando valores do formulário
+
+    const form = document.querySelector("#form-adiciona")
+    const nome = form.nome.value
+    const peso = form.peso.value
+    const altura = form.altura.value
+    const gordura = form.gordura.value
+
+    // Criando elementos
+
+    const pacienteTR = document.createElement("tr")
+    const nomeTd = document.createElement("td")
+    const pesoTd = document.createElement("td")
+    const alturaTd = document.createElement("td")
+    const gorduraTd = document.createElement("td")
+    const imcTd = document.createElement("td")
+
+    // pegando conteúdo dos inputs
+
+    nomeTd.textContent = nome
+    pesoTd.textContent = peso
+    alturaTd.textContent = altura
+    gorduraTd.textContent = gordura
+    imcTd.textContent = imcTd
+    
+    // Colocando elementos como filho
+
+    pacienteTR.appendChild(nomeTd)
+    pacienteTR.appendChild(pesoTd)
+    pacienteTR.appendChild(alturaTd)
+    pacienteTR.appendChild(gorduraTd)
+
+    // Adiconando a tabela
+
+    const tabela = document.querySelector("#tabela-pacientes")
+    tabela.appendChild(pacienteTR)
 })
