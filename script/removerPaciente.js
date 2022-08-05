f9dc31903c6b0f4ebbfbdf30b302b4634418cbdf
ElivanLimaJunior <85586631+ElivanLimaJunior@users.noreply.github.com>
2022-08-05 20:15:55 -0300

    Removendo paciente inteiro
    Nesta aula vimos o uso da propriedade parentNode. Usamos ele para acessar a propriedade pai do elemento.
    Antes o evento era aplicado o filho, agora nós aplicamos o evento no filho mas executamos a função no pai dele.

--- script/removerPaciente.js
+++ script/removerPaciente.js
@@ -1,7 +1,16 @@
 const pacientesAll = document.querySelectorAll(".paciente")
 
-pacientesAll.forEach(paciente => {
-    paciente.addEventListener("dblclick", function(){
-        this.remove() // para eu ter acesso ao dono desse evento, preciso usar a palavra "this". nesse caso o evento vai execultar a ação e o evento está atrelado ao dono do evento que é o paciente.
-    })
-});
+const tabelap = document.querySelector("#tabela-pacientes")
+
+tabelap.addEventListener("dblclick", function(event){
+    var alvoEvento = event.target
+    var paiDoAlvo = alvoEvento.parentNode // ParentNode nós iremos acessar o pai do evento clicado. 
+    paiDoAlvo.remove()
+    
+})
+
+// pacientesAll.forEach(paciente => {
+//     paciente.addEventListener("dblclick", function(){
+//         this.remove() // para eu ter acesso ao dono desse evento, preciso usar a palavra "this". nesse caso o evento vai execultar a ação e o evento está atrelado ao dono do evento que é o paciente.
+//     })
+// });
