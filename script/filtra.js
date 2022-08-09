8f7e4d907a4b1c0555876147326c737ca7c0fa2c
ElivanLimaJunior <85586631+ElivanLimaJunior@users.noreply.github.com>
2022-08-09 19:47:29 -0300

    Adicionando tag invisível

--- script/filtra.js
+++ script/filtra.js
@@ -2,11 +2,20 @@ const campoFiltro = document.querySelector('#filtrar-tabela')
 
 campoFiltro.addEventListener("input", function(){
     console.log(this.value)
-    const pacientes = document.querySelectorAll("paciente")
+    const pacientes = document.querySelectorAll(".paciente")
+
+    //Percorer todos os pacientes presentes
     for (let i = 0; i < pacientes.length; i++) {
         const paciente = pacientes[i];
         const tdNome = paciente.querySelector(".info-nome")
-        const nome = paciente.textContent
+        const nome = tdNome.textContent
+
+        //Verificação: Nome for diferente dentro do campoFiltro, ele irá adicionar a class invisivel nos pacientes. Caso tenha ele irá remover essa class.        
+        if(nome!= this.value){
+            paciente.classList.add("invisivel")
+        }else{
+            paciente.classList.remove("invisivel")
+        }
     }
         
 });
