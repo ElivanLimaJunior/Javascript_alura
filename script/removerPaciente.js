d9cb849d6942cfb6e443a95e913329b3d4aadd18
ElivanLimaJunior <85586631+ElivanLimaJunior@users.noreply.github.com>
2022-08-09 19:16:57 -0300

    Fim da aula 07
    Neste capítulo aprendemos:
    
    O novo evento de dblclick
    Como os eventos propagam atráves da página
    A delegação de eventos
    Como animar uma remoção com o setTimeout

--- script/removerPaciente.js
+++ script/removerPaciente.js
@@ -4,7 +4,14 @@ const tabelap = document.querySelector("#tabela-pacientes")
 
 tabelap.addEventListener("dblclick", function(event){
     // ParentNode nós iremos acessar o pai do evento clicado. 
-    event.target.parentNode.remove()
+    event.target.parentNode.classList.add("fadeOut")
+
+    //O javascript executa essas funções instataneamente, então precisamos por um time para ele executar a função de remover para assim podermos ver a animação de remoção que estamos adicionado.
+    //setTimeout() Pode receber dentro dele uma função, a função que passamos para ele é justamente a função de remover o paciente da nossa planilha. 
+    setTimeout(function(){
+        event.target.parentNode.remove()
+    },500) // apóso fechamento da função, nós passamos em Milisegundos o tempo de irá demorar para executar a fução dentro do timeout
+    
     
 })
 
